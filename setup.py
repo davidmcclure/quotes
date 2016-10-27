55ec0549adbc2c0ad04aace232829f5662445f91
David McClure <davidwilliammcclure@gmail.com>
2016-10-27 16:54:59 -0700

    In-memory SQLite working in Jupyter.

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name='quotes',
+    version='0.1.0',
+    description='How are texts quoted?',
+    url='https://github.com/davidmcclure/quotes',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@stanford.edu',
+    packages=find_packages(),
+
+)
