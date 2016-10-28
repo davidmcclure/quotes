d43894fa40a6c0c99706f7606cedca72c8ae41ac
David McClure <davidwilliammcclure@gmail.com>
2016-10-28 16:48:45 -0700

    Add tasks.py, with init_db.

--- quotes/models/base.py
+++ quotes/models/base.py
@@ -1,48 +1,11 @@
 
 
-from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.engine.url import URL
-from sqlalchemy.orm import sessionmaker, scoped_session
+
+from quotes.singletons import session
 
 
 class Base:
-
-    @classmethod
-    def connect(cls, url=None):
-
-        """
-        Set the database connection.
-        """
-
-        db_url = URL(**dict(
-            drivername='sqlite',
-            database=url,
-        ))
-
-        engine = create_engine(db_url)
-
-        # Fix transaction bugs in pysqlite.
-
-        @event.listens_for(engine, 'connect')
-        def connect(conn, record):
-            conn.isolation_level = None
-
-        @event.listens_for(engine, 'begin')
-        def begin(conn):
-            conn.execute('BEGIN')
-
-        factory = sessionmaker(bind=engine)
-
-        session = scoped_session(factory)
-
-        # Attach query shortcut.
-        cls.query = session.query_property()
-
-        # Create tables.
-        cls.metadata.create_all(engine)
-
-        return session
 
     def columns(self):
 
@@ -67,3 +30,5 @@ class Base:
 
 
 Base = declarative_base(cls=Base)
+
+Base.query = session.query_property()
