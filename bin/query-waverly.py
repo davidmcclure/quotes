4d9b8dd0c66f2bdf420660c2ea641865f2892858
David McClure <davidwilliammcclure@gmail.com>
2016-12-07 10:06:45 -0800

    Add Waverly job.

---
+++ bin/query-waverly.py
@@ -0,0 +1,16 @@
+
+
+from quotes.singletons import config
+from quotes.jobs.query_stacks import QueryStacks
+
+
+if __name__ == '__main__':
+
+    job = QueryStacks(
+        corpus_dirs=config['stacks_dirs'],
+        result_dir=config['result_dir'],
+        text_path=config['text_paths']['waverly'],
+        text_slug='waverly',
+    )
+
+    job()
