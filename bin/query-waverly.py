b2ad72a75110e258088f43f77bf6562a315d32e2
David McClure <davidwilliammcclure@gmail.com>
2016-12-14 15:16:19 -0800

    Update the query-waverly job.

--- bin/query-waverly.py
+++ bin/query-waverly.py
@@ -7,10 +7,10 @@ from quotes.jobs.query_stacks import QueryStacks
 if __name__ == '__main__':
 
     job = QueryStacks(
-        corpus_dirs=config['stacks_dirs'],
+        chadh_corpus_dir=config['chadh_corpus_dir'],
+        chadh_slug='Chadwyck_British_1814_Scott_WaverleyorTis',
+        stacks_db_url=config['stacks_db_url'],
         result_dir=config['result_dir'],
-        text_path=config['text_paths']['waverly'],
-        text_slug='waverly',
     )
 
     job()
