9ab66f6035714a1b9aa565263138fa277ab08c69
David McClure <davidwilliammcclure@gmail.com>
2017-05-25 09:39:54 -0700

    Spike in a multiprocessing version for EC2.

---
+++ bin/ext-pl.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+
+import os
+import ujson
+import uuid
+import click
+
+from datetime import datetime as dt
+from functools import partial
+from multiprocessing import Pool
+
+from quotes.models import BPOArticle, QueryText
+from quotes.text import Text
+from quotes.utils import mem_pct
+
+
+def align_year(slug, year):
+    """Given a year, align the text identifier by slug with all BPO articles in
+    the passed year.
+
+    Args:
+        slug (str)
+        year (int)
+    """
+    # Hydrate the query text.
+    text = QueryText.query.filter_by(slug=slug).one()
+    a = Text(text.text)
+
+    # Load articles in year.
+    articles = BPOArticle.query.filter_by(year=year)
+
+    matches = []
+    for i, article in enumerate(articles):
+
+        try:
+
+            b = Text(article.text)
+
+            # Align article -> text.
+            # matches = a.match(b)
+
+            # Record matches.
+            for m in a.match(b):
+
+                a_prefix, a_snippet, a_suffix = a.snippet(m.a, m.size)
+                b_prefix, b_snippet, b_suffix = b.snippet(m.b, m.size)
+
+                matches.append(dict(
+
+                    a_id=text.id,
+                    b_id=article.record_id,
+
+                    a_start=m.a,
+                    b_start=m.b,
+                    size=m.size,
+
+                    a_prefix=a_prefix,
+                    a_snippet=a_snippet,
+                    a_suffix=a_suffix,
+
+                    b_prefix=b_prefix,
+                    b_snippet=b_snippet,
+                    b_suffix=b_suffix,
+
+                ))
+
+        except Exception as e:
+            print(e)
+
+        if i % 1000 == 0:
+            print('align', year, i, mem_pct(), dt.now().isoformat())
+
+    return matches
+
+
+@click.command()
+@click.argument('slug')
+@click.argument('result_dir', type=click.Path())
+def main(slug, result_dir):
+    """Align a text against all of BPO.
+
+    Args:
+        slug (str)
+        result_dir (str)
+    """
+    years = BPOArticle.years()
+
+    worker = partial(align_year, slug)
+
+    with Pool() as pool:
+
+        results = pool.imap_unordered(worker, years)
+
+        for matches in results:
+
+            fname = str(uuid.uuid4())
+
+            path = os.path.join(result_dir, '{}.json'.format(fname))
+
+            with open(path, 'w') as fh:
+                ujson.dump(matches, fh)
+
+
+if __name__ == '__main__':
+    main()
